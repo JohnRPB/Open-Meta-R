0889ffea4a4fc361fd284e7caa3846179f02be95
Johann <baptistajohann@gmail.com>
2018-01-27 18:27:20 -0800

    added plot function

---
+++ R/test_plot.R
@@ -0,0 +1,13 @@
+#' A test function
+#'
+#' This function was created to allow you to test the OpenCPU service
+#' @param x x-variable
+#' @param y y-variable
+#' @keywords ocpu
+#' @export
+#' @examples
+#' test_plot()
+
+test_plot <- function(x, y) {
+  plot(x, y);
+}
