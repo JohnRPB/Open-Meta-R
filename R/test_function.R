d1873b31489ae51e254ce42aed958ed56f57b004
Johann <baptistajohann@gmail.com>
2018-01-27 17:24:03 -0800

    first commit

---
+++ R/test_function.R
@@ -0,0 +1,12 @@
+#' A test function
+#'
+#' This function was created to allow you to test the OpenCPU service
+#' @param num Can you type a number?
+#' @keywords ocpu
+#' @export
+#' @examples
+#' test_function()
+
+test_function <- function(num) {
+  num+2
+}
