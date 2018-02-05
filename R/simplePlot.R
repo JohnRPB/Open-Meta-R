78f5a2a3821d776b84f536211037f1f529922603
Johann <baptistajohann@gmail.com>
2018-02-04 16:07:23 -0800

    attempted plotly

--- R/simplePlot.R
+++ R/simplePlot.R
@@ -6,12 +6,41 @@
 #' @export
 #' @examples
 #' simplePlot()
+#' NOTE: for ggplot() to work, "import(ggplot2)" must be in the NAMESPACE file
 
-simplePlot <- function(json) {
+simplePlot <- function(json = "~/Documents/SoftwareDevelopment/VikingCodeSchool/final_project/openMeta/src/dbstudiesJSON.json") {
+  
+  #'------------------------------------------------------------------------
+  #' DATA FRAME transformations
+  #' -----------------------------------------------------------------------
+  
   df <- data.frame(jsonlite::fromJSON(json))
+  
+  #' Ensures that only studies selected for analysis are available
   df <- df[df$active == TRUE,]
   
-  ## ggplot2::qplot(df$x, df$y, geom = "line")
-  ggplot2::ggplot(data = df, ggplot2::aes(x = sampleSize, y = testStatVal)) + ggplot2::geom_point()
+  #'------------------------------------------------------------------------
+  #' DISPLAY PROPERTIES
+  #' -----------------------------------------------------------------------
+  
+  
+  
+  #'------------------------------------------------------------------------
+  #' PACKAGE FUNCTIONS
+  #' -----------------------------------------------------------------------
+  #' If your module has any dependencies, it is recommended you require the
+  #' functions from those dependencies that you need, below, like so:
+  #' functionNeeded <- packageUsed::functionNeeded
+  
+  ggplot <- ggplot2::ggplot
+  aes <- ggplot2::aes
+  geom_point <- ggplot2::geom_point
+  
+  #'------------------------------------------------------------------------
+  #' CUSTOM MODULE CODE
+  #'------------------------------------------------------------------------
+  #' Put your primary code below
+  
+  ggplot(data = df, aes(x = sampleSize, y = testStatVal)) + geom_point()
 }
 
